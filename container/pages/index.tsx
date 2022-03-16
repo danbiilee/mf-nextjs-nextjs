fff4cf8af30b6b40508637dc962ba0227fce9477
danbiilee <danbi.db@gmail.com>
2022-03-16 20:36:09 +0900

    tailwind css 적용

--- container/pages/index.tsx
+++ container/pages/index.tsx
@@ -1,19 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from './index.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <main className={styles.main}>
       <Head>
         <title>Microfrontends Demo</title>
         <meta name="description" content="Demo for Microfrontends using Module Federation" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <button>로그인</button>
-        <button>회원가입</button>
-      </main>
-    </div>
+      <button>로그인</button>
+      <button>회원가입</button>
+    </main>
   );
 }
