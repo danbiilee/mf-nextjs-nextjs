80ad3770326296992cbb0ae0dbd64cae0cdcfa27
danbiilee <danbi.db@gmail.com>
2022-03-16 14:52:48 +0900

    shll next.config 수정

--- shell/next.config.js
+++ shell/next.config.js
@@ -2,27 +2,29 @@
  * @type {import('next').NextConfig}
  */
 
+const mfConfig = {
+  remoteType: 'var',
+  remotes: {
+    app1: 'app1',
+    app2: 'app2',
+  },
+  shared: {
+    react: {
+      // Notice shared ARE eager here.
+      eager: true,
+      singleton: true,
+      requiredVersion: false,
+    },
+  },
+};
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['upload.wikimedia.org'],
+  },
   webpack(config, options) {
-    config.plugins.push(
-      new options.webpack.container.ModuleFederationPlugin({
-        remoteType: 'var',
-        remotes: {
-          app1: 'app1',
-          app2: 'app2',
-        },
-        shared: {
-          react: {
-            // Notice shared ARE eager here.
-            eager: true,
-            singleton: true,
-            requiredVersion: false,
-          },
-        },
-      }),
-    );
-
+    config.plugins.push(new options.webpack.container.ModuleFederationPlugin(mfConfig));
     return config;
   },
 };
